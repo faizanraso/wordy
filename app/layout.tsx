2eab65caa4893e1e669aec2142ca1f6863ccb29e
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-08-23 22:49:05 -0400

    Project rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Wordies',
-  description: 'A game to see how wordy you are',
-}
+  title: "Wordy",
+  description: "A game to see how wordy you are",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
