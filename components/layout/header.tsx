1c4801714eac0b4be45775ee570014735772dd8c
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-09-03 20:50:57 -0400

    Complete

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link";
 
-import "@/public/logo.png";
-import { Dialog } from "../ui/shadcn/dialog";
+import Info from "../ui/info";
+import Stats from "../ui/stats";
 import Profile from "../ui/profile";
+import "@/public/logo.png";
 
 export default function Header() {
   return (
@@ -20,18 +21,8 @@ export default function Header() {
 
           <div className="flex items-center gap-4">
             <div className="flex gap-1">
-              <Link href={"/"}>
-                <svg
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  width={32}
-                  height={32}
-                  className="hover:bg-gray-200 rounded-md transition duration-100 p-1"
-                >
-                  <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z" />
-                  <path d="M11 11h2v6h-2zm0-4h2v2h-2z" />
-                </svg>
-              </Link>
+              <Info />
+              <Stats />
               <Profile />
             </div>
           </div>
