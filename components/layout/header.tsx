68f9eb70f587ec22a7e1882d4fe0aaaf5cb41453
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-09-03 20:46:37 -0400

    Set up modals

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import "@/public/logo.png";
 import { Dialog } from "../ui/shadcn/dialog";
+import Profile from "../ui/profile";
 
 export default function Header() {
   return (
@@ -31,47 +32,7 @@ export default function Header() {
                   <path d="M11 11h2v6h-2zm0-4h2v2h-2z" />
                 </svg>
               </Link>
-              <Dialog />
-              <Link href={"/"}>
-                <svg
-                  width={32}
-                  height={32}
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  color="#000000"
-                  className="hover:bg-gray-200 rounded-md transition duration-100 p-1"
-                >
-                  <path
-                    d="M15 19H9V8.6a.6.6 0 01.6-.6h4.8a.6.6 0 01.6.6V19zM15 5H9M20.4 19H15v-3.9a.6.6 0 01.6-.6h4.8a.6.6 0 01.6.6v3.3a.6.6 0 01-.6.6zM9 19v-5.9a.6.6 0 00-.6-.6H3.6a.6.6 0 00-.6.6v5.3a.6.6 0 00.6.6H9z"
-                    stroke="#000000"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>
-                </svg>
-              </Link>
-              <Link href={"/"}>
-                <svg
-                  width={32}
-                  height={32}
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  color="#000000"
-                  className="hover:bg-gray-200 rounded-md transition duration-100 p-1"
-                >
-                  <path
-                    d="M5 20v-1a7 7 0 017-7v0a7 7 0 017 7v1M12 12a4 4 0 100-8 4 4 0 000 8z"
-                    stroke="#000000"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>
-                </svg>
-              </Link>
+              <Profile />
             </div>
           </div>
         </div>
