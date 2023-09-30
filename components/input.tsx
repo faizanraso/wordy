0da80b7455dd1e0c5d1507c5dbf0658ad5d49791
Faizan Rasool <faizanraso@outlook.com>
2023-09-30 11:13:37 -0400

    update

--- components/input.tsx
+++ components/input.tsx
@@ -20,7 +20,7 @@ export default function Input(props: { setUserWords: any; userWords: any }) {
   let reset: NodeJS.Timeout | undefined;
 
   const { data, error, isLoading } = useSWR("/api/getLevel", fetcher);
-
+  console.log(error);
   useEffect(() => {
     async function startGame() {
       setLevelData(data[Math.floor(Math.random() * data.length)]);
